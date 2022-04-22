a15b479cb6d143f54ccd36744b90a492c6afd7ec
HP <HP@LAPTOP-OBACGJG1>
2022-04-22 15:57:51 +0530

    commit10

---
+++ src/main/java/com/springboot6772/SpringBoot6772Application.java
@@ -0,0 +1,15 @@
+package com.springboot6772;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6772Application 
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringBoot6772Application.class, args);
+	}
+
+}
